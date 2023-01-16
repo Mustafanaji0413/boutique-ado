21508929790dc3ae11709dea8e826c50786f1122
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-01-16 17:11:48 +0000

    added products and views

--- products/models.py
+++ products/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Category(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Cateories'
-
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -25,9 +25,6 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
 
     def __str__(self):
         return self.name
